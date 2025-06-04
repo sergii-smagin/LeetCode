2e77cecc817caded2507734cf924c21b30814f9c
Sergii Smagin <mail.unforgiven@gmail.com>
2025-06-03 21:53:51 -0700

    Added 'Remove Duplicates from Sorted Array' (26) solution.

---
+++ src/RemoveDuplicatesFromSortedArray_26/Solution.java
@@ -0,0 +1,15 @@
+package RemoveDuplicatesFromSortedArray_26;
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int index = 1;
+        for(int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] != nums[i]) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        return index;
+    }
+}
