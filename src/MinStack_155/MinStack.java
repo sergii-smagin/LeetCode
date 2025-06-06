40df5a98224fe4af5d83aced1591f918934cbd28
Sergii Smagin <mail.unforgiven@gmail.com>
2025-06-05 21:32:33 -0700

    Added 'Min Stack' (155) solution.

---
+++ src/MinStack_155/MinStack.java
@@ -0,0 +1,35 @@
+package MinStack_155;
+
+import java.util.Stack;
+
+public class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int currentVal = stack.pop();
+        if (currentVal == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
