bf74b466dbc2a2bddd372b48ca7e27b4f34960ac
Sergii Smagin <mail.unforgiven@gmail.com>
2025-08-02 16:17:23 -0600

    Added 'Valid Parentheses' (20) solution.

---
+++ src/ValidParentheses_20/Solution.java
@@ -0,0 +1,24 @@
+package ValidParentheses_20;
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty() ||
+                        (c == ')' && stack.pop() != '(' ) ||
+                        (c == '}' && stack.pop() != '{' ) ||
+                        (c == ']' && stack.pop() != '[' )) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
